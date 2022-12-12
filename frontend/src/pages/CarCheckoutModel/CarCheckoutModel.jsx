f18ca7fecd4c0b4d620916eab05dc6de11d44c19
hemsingh780 <hemdsinghdhoni_bca18_21@its.edu.in>
2022-12-12 20:04:47 +0530

    compelete CarCheckoutModel

--- frontend/src/pages/CarCheckoutModel/CarCheckoutModel.jsx
+++ frontend/src/pages/CarCheckoutModel/CarCheckoutModel.jsx
@@ -3,75 +3,110 @@ import BrandingWatermarkIcon from '@mui/icons-material/BrandingWatermark';
 import LocalGasStationIcon from '@mui/icons-material/LocalGasStation';
 import MinorCrashIcon from '@mui/icons-material/MinorCrash';
 import PolicyIcon from '@mui/icons-material/Policy';
-
-
+import './CarCheckoutModels.css'
+import CircleIcon from '@mui/icons-material/Circle';
 
 const CarCheckoutModel = () => {
   return (
-    <div>
-      <div>
-          <div>
+    <div className='checkoutModel-Main'>
+      <div className='checkoutModel-Main1'>
+          <div className='checkoutModel-Main11'>
               <div>
-                <div>
-                    name of car
+                <div className='checkoutModel-Main11-CarName'>
+                   Toyota Glanza MT Petrol
                 </div>
-                <div>details of car</div>
+                <div style={{
+                  display:"flex",
+                }}>
+                   <div style={{
+                    marginRight:"8%"
+                   }}>
+                      Manual
+                   </div>
+                   <div>
+                      Petrol                   
+                   </div>
+                </div>
               </div>
-              <div>Image</div>
+              {/* <div>
+                <img src='https://hemsingh780.github.io/hosted-assest/RenualtKwid.png' alt='carname'/>
+              </div> */}
           </div>
           <div>
-              <div>
+            <div className="timeCarCheck">
+                <div>
+                  <CircleIcon  style={{
+                    color:"green",
+                    fontSize:"16px"
+                  }}/>
+                </div>
+                <div>
                 starting time day 
-              </div>
-              <div>
+                </div>
+            </div>
+            <div  className="timeCarCheck">
+                <div>
+                   <CircleIcon   
+                   style={{
+                    color:"red",
+                    fontSize:"16px"
+                  }}
+                   />
+                </div>
+                <div>
                 ending day and time 
-              </div>
+                </div>
+            </div>
+
           </div>
 
       </div>
-      <div>
-              <div>
+      <div className='checkoutModel-Main2'>
+              <div className='checkoutModel-Main2-h'>
               Keep In Mind
               </div>
-              <div>
+              <div className='keepInMindInnerDiv1'>
                 <div>
                   <MinorCrashIcon />
                 </div>
-                <div>
+                <div className='keepInMindInnerDiv1Detail'>
                   Driving License Mandatory
                 </div>
               </div>
-              <div>
+              <div className='keepInMindInnerDiv1'>
                 <div>
                   <LocalGasStationIcon />
                  </div>
-                <div>
-                 <div>Fare & Fuel Policy</div>
+                <div  className='keepInMindInnerDiv1Detail'>
+                  <div>Fare & Fuel Policy</div>
                   <div>Fare is inclusive of all taxe . kindly return the car at the same fuel level as the trip start time</div>
+                  <a href='#'>See Details</a>
                 </div>
               </div>
-              <div>
+              <div className='keepInMindInnerDiv1'>
                   <div>
                     <MinorCrashIcon />
                   </div>
-                    <div>
+                    <div className='keepInMindInnerDiv1Detail'>
                         <div>Cancellation Policy</div>
                         <div>Hassle free cancellation  and refunds at nominal charges</div>
+                        <a href='#'>See Details</a>
                     </div>
               </div>
-              <div>
+              <div className='keepInMindInnerDiv1'>
                 <div>
                   <PolicyIcon />
                 </div>
-                  <div>
+                  <div className='keepInMindInnerDiv1Detail'>
                         <div>Agreement Policy</div>
                         <div>I hereby agree to the terms and conditions of the Lease Agreement with Host</div>
+                        <a href='#'>See Details</a>
                   </div>
               </div>
           </div>
 
-          <div>
-              <div>Price</div>
+          <div className='checkoutModel-Main3'>
+              <div><span>₹</span>1234</div>
               <div>Proceed to pay "price" </div>
           </div>
     </div>
